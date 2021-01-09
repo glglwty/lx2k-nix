0ac6e53b7741a0ee1f7aa3e336733d8288529d44
Linus Heckemann <git@sphalerite.org>
2021-01-09 17:05:23 +0100

    Cross-compile as appropriate

--- release.nix
+++ release.nix
@@ -1,7 +1,10 @@
-{ pkgs ? import <nixpkgs> {
+{ system ? builtins.currentSystem
+, pkgs ? import <nixpkgs> {
     overlays = [
       (import ./overlay.nix)
     ];
+    inherit system;
+    ${if system == "aarch64-linux" then null else "crossSystem"} = "aarch64-linux";
   },
   ddrSpeed ? 3200
 }:
