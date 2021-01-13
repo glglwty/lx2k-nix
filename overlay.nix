a5bbc32c2d7ae291f9289c1e2df374a9dc7358cf
Linus Heckemann <git@sphalerite.org>
2021-01-13 08:08:56 +0100

    Add lx2k_mainline kernel

--- overlay.nix
+++ overlay.nix
@@ -16,6 +16,18 @@ self: super: {
 
   linuxPackages_lx2k = self.linuxPackagesFor self.linux_lx2k;
 
+  linuxPackages_lx2k_mainline = super.linuxPackagesFor self.linux_lx2k_mainline;
+  linux_lx2k_mainline = self.buildLinux {
+    src = super.fetchFromGitHub {
+      owner = "SolidRun";
+      repo = "linux-stable";
+      rev = "bc1160fd43de6bc9a09e25c027a057e1376e5b9a";
+      sha256 = "1cq36vpsd68g144gn7f3jjkl2bwibqv7nrrrjgvkdj1lfijcwm14";
+    };
+    version = "5.10.5";
+    kernelPatches = [];
+  };
+
   lx2k = self.lib.makeScope self.newScope (self: with self; {
     uboot = self.callPackage ./u-boot { };
 
