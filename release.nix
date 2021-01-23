d0c859c216bed890ea372140edfea6e922e41483
Linus Heckemann <git@sphalerite.org>
2021-01-23 19:23:14 +0100

    Use 5.10 kernel for SD image

--- release.nix
+++ release.nix
@@ -21,13 +21,13 @@
     imports = [ (pkgs.path + /nixos/modules/installer/cd-dvd/sd-image-aarch64.nix) ];
 
     # use vendor kernel
-    boot.kernelPackages = pkgs.linuxPackages_lx2k;
+    boot.kernelPackages = pkgs.linuxPackages_lx2k_mainline;
 
     # disable anything we don't need, like zfs
     boot.initrd.supportedFilesystems = lib.mkForce [ "ext4" ];
     boot.supportedFilesystems = lib.mkForce [ "ext4" ];
 
     # take from upstream
-    boot.kernelParams = lib.mkForce [ "console=ttyAMA0,115200" "earlycon=pl011,mmio32,0x21c0000" "default_hugepagesz=1024m" "hugepagesz=1024m" "hugepages=2" "pci=pcie_bus_perf" ];
+    boot.kernelParams = lib.mkForce [ "console=ttyAMA0,115200" "earlycon=pl011,mmio32,0x21c0000" "pci=pcie_bus_perf" ];
   })).config.system.build.sdImage;
 }
