637893f84066a07527468db2fca6ede7f74e16f6
Linus Heckemann <git@sphalerite.org>
2021-01-23 20:40:25 +0100

    add arm-smmu.disable_bypass=0 to fix iso-image boot

--- release.nix
+++ release.nix
@@ -28,6 +28,9 @@
     boot.supportedFilesystems = lib.mkForce [ "ext4" ];
 
     # take from upstream
-    boot.kernelParams = lib.mkForce [ "console=ttyAMA0,115200" "earlycon=pl011,mmio32,0x21c0000" "pci=pcie_bus_perf" ];
+    boot.kernelParams = lib.mkForce [
+      "console=ttyAMA0,115200" "earlycon=pl011,mmio32,0x21c0000" "pci=pcie_bus_perf"
+      "arm-smmu.disable_bypass=0" # TODO: remove once firmware supports it
+    ];
   })).config.system.build.isoImage;
 }
