6337d9179495817374a03af0e6ecc1c6d4c727e9
Linus Heckemann <git@sphalerite.org>
2021-06-02 10:13:51 +0200

    linux_lx2k: 5.10.5 -> 5.10.23

--- overlay.nix
+++ overlay.nix
@@ -1,13 +1,14 @@
 self: super: {
   linuxPackages_lx2k = super.linuxPackagesFor self.linux_lx2k;
   linux_lx2k = self.buildLinux {
+    # nix-prefetch-github https://github.com/SolidRun/linux-stable linux-5.10.y-cex7
     src = super.fetchFromGitHub {
       owner = "SolidRun";
       repo = "linux-stable";
-      rev = "bc1160fd43de6bc9a09e25c027a057e1376e5b9a";
-      sha256 = "1cq36vpsd68g144gn7f3jjkl2bwibqv7nrrrjgvkdj1lfijcwm14";
+      rev = "4c71505dce5df9254daacb96c7a869741289a461";
+      sha256 = "1mqv6xvijrafkjf03bwjvfrcvd4h4576zbv8jpipfjzcxg6894zf";
     };
-    version = "5.10.5";
+    version = "5.10.23";
     kernelPatches = [ ];
     structuredExtraConfig = with super.lib.kernel; {
       CGROUP_FREEZER = yes;
